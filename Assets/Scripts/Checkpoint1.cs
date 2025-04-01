519ab673b837515ebb6a41df69e38bdd6a5f9bbc
VincentG2004 <vincent.gaudet.bernier@gmail.com>
2025-04-01 14:58:58 -0400

    Début d'instantiation

--- Assets/Scripts/Checkpoint1.cs
+++ Assets/Scripts/Checkpoint1.cs
@@ -9,6 +9,9 @@ public class Checkpoint1 : MonoBehaviour
     public float trous = 0;
     public bool hit1 = false;
     public TextMeshProUGUI trousTexte;
+    [SerializeField] GameObject _objet;
+    Vector3 _position;
+    Quaternion _rotation;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +33,15 @@ public class Checkpoint1 : MonoBehaviour
     /// <param name="other">The other Collider involved in this collision.</param>
     private void OnTriggerEnter(Collider other)
     {
+        int i = 1;
+        
+        while(i < 7){
+        _position = new Vector3(Random.Range(8f, 18f), 402f, Random.Range(214f, 215f));
+        _rotation = Random.rotation;
+        Instantiate(_objet, _position, _rotation);
+        i += 1;
+        }
+
         if(hit1==false){
             trous += 1;
             hit1 = true;
